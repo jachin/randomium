5800ef082472db1ad8fd4dcf014d9bd0f3c51eaf
Jachin Rupe <jachin.rupe@gmail.com>
2014-12-31 03:42:59 -0500

    Adding some better metadata.

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
   name='randomium',
   version='0.1',
   description='Tools for generating random things.',
+  long_description=readme(),
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Testing',
+  ],
   url='http://github.com/jachin/randomium',
   author='Jachin Rupe',
   author_email='jachin@jachin.rupe.name',
